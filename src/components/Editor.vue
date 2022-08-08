8ae4f416739fb5d8ac7e2a8f3bc335f052c84d19
Max <max@nextcloud.com>
2022-08-08 11:18:17 +0200

    fix: do not track state in the syncService
    We can serialize based on the current state of the editor alone.
    
    Signed-off-by: Max <max@nextcloud.com>

--- src/components/Editor.vue
+++ src/components/Editor.vue
@@ -336,13 +336,9 @@ export default {
 				shareToken: this.shareToken,
 				filePath: this.relativePath,
 				forceRecreate: this.forceRecreate,
-				serialize: (document) => {
-					if (this.isRichEditor) {
-						return (createMarkdownSerializer(this.$editor.schema)).serialize(document)
-					}
-					return serializePlainText(this.$editor)
-
-				},
+				serialize: this.isRichEditor
+					? () => createMarkdownSerializer(this.$editor.schema).serialize(this.$editor.state.doc)
+					: () => serializePlainText(this.$editor),
 			})
 
 			this.listenSyncServiceEvents()
@@ -495,11 +491,7 @@ export default {
 						session: this.currentSession,
 						content,
 						onCreate: ({ editor }) => {
-							this.$syncService.state = editor.state
 							this.$syncService.startSync()
-						},
-						onUpdate: ({ editor }) => {
-							this.$syncService.state = editor.state
 						},
 						extensions: [
 							Collaboration.configure({
@@ -517,7 +509,6 @@ export default {
 
 					this.listenEditorEvents()
 
-					this.$syncService.state = this.$editor.state
 				})
 
 		},
@@ -536,7 +527,6 @@ export default {
 
 		onSync({ steps, document }) {
 			this.hasConnectionIssue = false
-			this.$syncService.state = this.$editor.state
 			this.updateLastSavedStatus()
 			this.$nextTick(() => {
 				this.$emit('sync-service:sync')
